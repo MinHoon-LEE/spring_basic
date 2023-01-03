95e33844ccd1785c25f5b43b5df7cfe76cabd929
MinHoon-LEE <lucky3749@naver.com>
2023-01-03 14:39:03 +0900

    2003-01-03_spring

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store= new HashMap<>();
+    // 실무 에서는 Concurrent Hash Map 사용 해야 됨 //
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(),member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
