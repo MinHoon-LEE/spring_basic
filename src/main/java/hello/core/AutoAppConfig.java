580d2fdaab3b18bcaa2734bb9d3a8fea3d9941b0
MinHoon-LEE <lucky3749@naver.com>
2023-01-10 17:38:23 +0900

    2023-01-10

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,15 +1,24 @@
 package hello.core;
 
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+     //   basePackages = "hello.core.member";
         // AppConfig 를 안지울 거기 때문에 예외로 등록 못하게 한다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
 
 }
